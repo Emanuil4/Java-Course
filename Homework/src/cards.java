98e80e7df06bb23a7f74841b103e990986888955
Emanuil4 <emi95@abv.bg>
2015-05-02 10:05:00 +0300

    На мен не ми работи, но не мога да си намеря грешката

---
+++ Homework/src/cards.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class cards {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in,"UTF-8");
+		System.out.print("Please enter a card:");
+		String a= input.nextLine();
+		if(a=="2"){
+			System.out.println("Valid card!");
+		}
+		else if(a=="3"){
+			System.out.println("Valid card!");
+		}
+		else if(a=="4"){
+			System.out.println("Valid card!");
+		}
+		else if(a=="5"){
+			System.out.println("Valid card!");
+		}
+		else if(a=="6"){
+			System.out.println("Valid card!");
+		}
+		else if(a=="7"){
+			System.out.println("Valid card!");
+		}
+		else if(a=="8"){
+			System.out.println("Valid card!");
+		}
+		else if(a=="9"){
+			System.out.println("Valid card!");
+		}
+		else if(a=="10"){
+			System.out.println("Valid card!");
+		}
+		else if(a=="J" || a=="j" ){
+			System.out.println("Valid card!");
+		}
+		else if(a=="Q" || a=="q"){
+			System.out.println("Valid card!");
+		}
+		else if(a=="K" || a=="k"){
+			System.out.println("Valid card!");
+		}
+		else if(a=="A"|| a=="a" ){
+			System.out.println("Valid card!");
+		}
+		else{
+			System.out.println("Invalid card!");
+		}
+		}
+	}
+
+
