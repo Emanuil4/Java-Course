99c42718599f5430062b30baae9f30b6b46975f6
Emanuil4 <emi95@abv.bg>
2015-06-13 10:58:40 +0300

    threadPool

---
+++ Lection14_Homework/src/threadPool.java
@@ -0,0 +1,20 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+public class threadPool {
+
+	public static void main(String[] args) {
+		ExecutorService executor = Executors.newFixedThreadPool(7);
+		for (int i = 0; i < 5; i++) {
+			Runnable worker = new WorkerThread("" + i);
+			executor.execute(worker);
+		}
+		executor.shutdown();
+		while (!executor.isTerminated()) {
+		}
+
+		System.out.println("The thread complete");
+	}
+}
+
+
+
---
+++ Lection14_Homework/src/workerThread.java
@@ -0,0 +1,20 @@
+class WorkerThread implements Runnable {
+	private String message;
+
+	public WorkerThread(String s) {
+		this.message = " The thread is started";
+	}
+
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + message);
+		processMessage();
+	}
+
+	private void processMessage() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
